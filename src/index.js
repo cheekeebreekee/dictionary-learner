bd788be67c36a52422fc50c7364c1ef6bc889600
Yahor Simak <simak_yahor@mail.ru>
2018-01-10 22:37:48 +0300

    add implementain of dictionary learner task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function findRotationPoint(words) {
-  // your solution here
+  var leftPage = 0,
+      rightPage = words.length - 1;
+    while (words[leftPage] > words[rightPage]) {
+        var middlePage = Math.floor((leftPage + rightPage) / 2);
+        if (words[rightPage] < words[middlePage]) {
+            leftPage = middlePage + 1;
+        } else {
+            rightPage = middlePage;
+        }
+    }
+    return leftPage;
 }
